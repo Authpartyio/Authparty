eb0997796ff0b1fbb58e683c0f5712d4bcbf3b78
Matthew Reichardt <me@matthewreichardt.com>
2016-11-05 16:13:42 -0500

    Changed config vars.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :clef, 'ee62d01869399bd52e848568c57cff00', '4df776f848523975ed73ef440b797241',
+  provider :clef, ENV['APP_ID'], ENV['APP_SECRET'],
   {:provider_ignores_state => true}
 end
