10d2d841fe09fb6693e1f957d6bd409a38049079
Matthew Reichardt <me@matthewreichardt.com>
2016-11-04 21:19:57 -0500

    Trial fix.

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -3,7 +3,7 @@ module AccountsHelper
     Account.find(session[:user])
   end
 
-  def logged_in?
+  def logged_in
     if session[:user] != nil
       account = Account.find(session[:user])
       if session[:logged_in_at] > account.logged_out_at
