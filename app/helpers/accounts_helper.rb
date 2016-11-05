ee7c4cca35895674a6b0604c0903ed23373fc428
Matthew Reichardt <me@matthewreichardt.com>
2016-11-05 16:20:08 -0500

    Fixed first time login error.

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -6,10 +6,12 @@ module AccountsHelper
   def logged_in
     if session[:user] != nil
       account = Account.find(session[:user])
-      if session[:logged_in_at] > account.logged_out_at
-        return true
-      else
-        return false
+      if account.logged_out_at != nil
+        if session[:logged_in_at] < account.logged_out_at
+          return true
+        else
+          return false
+        end
       end
     else
       return false;
